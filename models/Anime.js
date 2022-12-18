cadab06f415acf9792e4ee9586d5e3c2cc382627
amrsalahuddin <ar.slhdn@gmail.com>
2022-12-18 14:38:01 +0200

    Added Base Server

--- models/Anime.js
+++ models/Anime.js
@@ -3,7 +3,7 @@ const { pool } = require('./db');
 
 
 class ANIME {
-    async insertAnime(animeName, authorId, studioId, singerId, genre, rate, episodes, yearPub, imgLink) {
+    async insertAnime(animeName, authorId, studioId, genre, rate, episodes, yearPub, imgLink) {
 
         //const checkNo = await pool.query(`SELECT COUNT(anime_name) where anime_name =${animeName}`);
         // console.log(checkNo);
@@ -12,7 +12,7 @@ class ANIME {
 
             const res = await pool.query(
 
-                `INSERT INTO Anime (anime_name,author_id,studio_id,singer_id,genre,rate,episodes,year_published,img_link) values('${animeName}',${authorId},${studioId},${singerId},'${genre}',${rate},${episodes},${yearPub},'${imgLink}');`);
+                `INSERT INTO Anime (anime_name,author_id,studio_id,genre,rate,episodes,year_published,img_link) values('${animeName}',${authorId},${studioId},'${genre}',${rate},${episodes},${yearPub},'${imgLink}');`);
             return 1;
         }
         catch (error) {
@@ -51,10 +51,10 @@ class ANIME {
 
                 `select studio_name ,studio.id from STUDIO , ANIME where anime.id=${animeId} AND studio.id = studio_id
                    `)
-            const singer = await pool.query(
-
-                `select singer_name ,singer.id from SINGER , ANIME where anime.id=${animeId} AND SINGER.id = singer_id
-                       `)
+            /* const singer = await pool.query(
+ 
+                 `select singer_name ,singer.id from SINGER , ANIME where anime.id=${animeId} AND SINGER.id = singer_id
+                        `)*/
             const character = await pool.query(
 
                 `SELECT * from character,va where anime_id=${animeId} AND character.va_id =va.va_id `)
@@ -66,7 +66,7 @@ class ANIME {
             const episodes = await pool.query(
 
                 `SELECT   episode_number,episode_link   from episodes where anime_id=${animeId} `)
-            let x = { 'anime': anime.rows[0], 'author': author.rows[0], 'studio': studio.rows[0], 'singer': singer.rows[0], "characters": character.rows, "awards": awards.rows, "episodes": episodes.rows };
+            let x = { 'anime': anime.rows[0], 'author': author.rows[0], 'studio': studio.rows[0], "characters": character.rows, "awards": awards.rows, "episodes": episodes.rows };
             return x;
         }
         catch (error) {
@@ -76,12 +76,12 @@ class ANIME {
 
     }
 
-    async updateAnime(animeName, authorId, studioId, singerId, genre, rate, episodes, yearPub, imgLink, animeId) {
+    async updateAnime(animeName, authorId, studioId, genre, rate, episodes, yearPub, imgLink, animeId) {
         try {
 
             const res = await pool.query(
 
-                `UPDATE Anime  SET (anime_name,author_id,studio_id,singer_id,genre,rate,episodes,year_published,img_link) = ('${animeName}',${authorId},${studioId},${singerId},'${genre}',${rate},${episodes},${yearPub},'${imgLink}') WHERE id='${animeId}';`);
+                `UPDATE Anime  SET (anime_name,author_id,studio_id,genre,rate,episodes,year_published,img_link) = ('${animeName}',${authorId},${studioId},'${genre}',${rate},${episodes},${yearPub},'${imgLink}') WHERE id='${animeId}';`);
             return 1;
         }
         catch (error) {
--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.get('/anime_details', (req, res) => {
 
 router.post('/insert/anime', (req, res, next) => {
     let pars = req.body;
-    anime.insertAnime(pars.animeName, pars.authorId, pars.studioId, pars.singerId, pars.genre, pars.rate, pars.episodes, pars.yearPub, pars.imgLink).then(data => {
+    anime.insertAnime(pars.animeName, pars.authorId, pars.studioId, pars.genre, pars.rate, pars.episodes, pars.yearPub, pars.imgLink).then(data => {
         res.json(data);
     });
 
